ec43410ce4efb3d6dbe0a91bca12f3b853e5d1d2
Rodrigo Roger <rodrigoroger002@gmail.com>
2023-03-28 21:56:54 -0300

    :fire:

---
+++ components/Cabecalho.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cabecalho = () => {
+    return (
+        <>
+        <div>Cabecalho</div>
+        <h1>Cabecalho</h1>
+        </>
+    )
+}
+
+export default Cabecalho
